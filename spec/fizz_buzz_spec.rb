276307f075cbd0c9e2f699bb0bbe378094265e9d
ken <kirie115@gmail.com>
2018-10-30 14:27:47 +0900

    first commit

---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../lib/fizz_buzz'
+
+describe 'fizz_buzz' do
+#   context '3または5の倍数ではない数字が引数の場合' do
+#     it '引数が返却される' do
+#       expect(fizz_buzz(1)).to eq('1')
+#       expect(fizz_buzz(2)).to eq('2')
+#       expect(fizz_buzz(4)).to eq('4')
+#     end
+#   end
+#
+#   context '3の倍数が引数の場合' do
+#     it 'Fizzが返却される' do
+#       expect(fizz_buzz(3)).to eq('Fizz')
+#       expect(fizz_buzz(6)).to eq('Fizz')
+#     end
+#   end
+#
+#   context '5の倍数が引数の場合' do
+#     it 'Buzzが返却される' do
+#       expect(fizz_buzz(5)).to eq('Buzz')
+#       expect(fizz_buzz(10)).to eq('Buzz')
+#     end
+#   end
+#
+#  context '3および5の倍数が引数の場合' do
+    it 'FizzBuzzが返却される' do
+      expect(fizz_buzz(15)).to eq('Fizz Buzz')
+    end
+#  end
+end
---
+++ spec/test_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/test'
+
+describe "caliculation" do
+  it '1+1 equal 2' do
+    expect(1 + 1).to eq 2
+  end
+end
